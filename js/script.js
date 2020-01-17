dd55bf2322e9672a56ef835214090b883002e999
Joao Patricio <joaopatr@amazon.com>
2020-01-17 11:22:29 +0100

    Commiting a bunch of sample data

--- js/script.js
+++ js/script.js
@@ -74,6 +74,28 @@
             mouseParallax('l9', c9left, c9top, x, y, 40);
         };
 
+        // Add smooth scrolling to all links
+        $(".smooth-scroll").on('click', function(event) {
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+              // Prevent default anchor click behavior
+              event.preventDefault();
+
+              // Store hash
+              var hash = this.hash;
+
+              // Using jQuery's animate() method to add smooth page scroll
+              // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+              $('html, body').animate({
+                scrollTop: $(hash).offset().top
+              }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+              });
+            } // End if
+        });
+
     };
 
     function mouseParallax(id, left, top, mouseX, mouseY, speed) {
@@ -85,56 +107,6 @@
         obj.style.top = top - (((mouseY - (parseInt(obj.offsetHeight) / 2 + top)) / containerHeight) * speed) + 'px';
     }
     /* ########################################### /hero parallax ############################################## */
-
-    // testimonial-slider
-    $('.testimonial-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        arrows: false,
-        adaptiveHeight: true
-    });
-
-
-    // clients logo slider
-    $('.client-logo-slider').slick({
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        dots: false,
-        arrows: false,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 400,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
 
     // Shuffle js filter and masonry
     var Shuffle = window.Shuffle;
